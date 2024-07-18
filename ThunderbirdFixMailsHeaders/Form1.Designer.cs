563f10d8c910a9c6a2428fa5ba524907fc8e3bd2
Kamil Mroczkowski <kmroczkowski@speed.gdynia.pl>
2024-07-18 09:25:19 +0200

    Init project.

---
+++ ThunderbirdFixMailsHeaders/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace ThunderbirdFixMailsHeaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbFile = new System.Windows.Forms.RadioButton();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btFile = new System.Windows.Forms.Button();
+            this.rbFolder = new System.Windows.Forms.RadioButton();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.btFolder = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbFile
+            // 
+            this.rbFile.AutoSize = true;
+            this.rbFile.Location = new System.Drawing.Point(12, 14);
+            this.rbFile.Name = "rbFile";
+            this.rbFile.Size = new System.Drawing.Size(75, 24);
+            this.rbFile.TabIndex = 0;
+            this.rbFile.TabStop = true;
+            this.rbFile.Text = "Fix file:";
+            this.rbFile.UseVisualStyleBackColor = true;
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(93, 13);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.ReadOnly = true;
+            this.tbFile.Size = new System.Drawing.Size(583, 26);
+            this.tbFile.TabIndex = 1;
+            // 
+            // btFile
+            // 
+            this.btFile.Location = new System.Drawing.Point(682, 12);
+            this.btFile.Name = "btFile";
+            this.btFile.Size = new System.Drawing.Size(47, 27);
+            this.btFile.TabIndex = 2;
+            this.btFile.Text = "...";
+            this.btFile.UseVisualStyleBackColor = true;
+            this.btFile.Click += new System.EventHandler(this.btFile_Click);
+            // 
+            // rbFolder
+            // 
+            this.rbFolder.AutoSize = true;
+            this.rbFolder.Location = new System.Drawing.Point(12, 46);
+            this.rbFolder.Name = "rbFolder";
+            this.rbFolder.Size = new System.Drawing.Size(162, 24);
+            this.rbFolder.TabIndex = 3;
+            this.rbFolder.TabStop = true;
+            this.rbFolder.Text = "Fix all files in folder:";
+            this.rbFolder.UseVisualStyleBackColor = true;
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(180, 45);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.ReadOnly = true;
+            this.tbFolder.Size = new System.Drawing.Size(496, 26);
+            this.tbFolder.TabIndex = 4;
+            // 
+            // btFolder
+            // 
+            this.btFolder.Location = new System.Drawing.Point(682, 45);
+            this.btFolder.Name = "btFolder";
+            this.btFolder.Size = new System.Drawing.Size(47, 27);
+            this.btFolder.TabIndex = 5;
+            this.btFolder.Text = "...";
+            this.btFolder.UseVisualStyleBackColor = true;
+            this.btFolder.Click += new System.EventHandler(this.btFolder_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbStatus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(717, 352);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Location = new System.Drawing.Point(6, 25);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.ReadOnly = true;
+            this.tbStatus.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbStatus.Size = new System.Drawing.Size(705, 321);
+            this.tbStatus.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 436);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(717, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btStart.Location = new System.Drawing.Point(548, 465);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(181, 36);
+            this.btStart.TabIndex = 9;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 473);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Author:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(80, 473);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Kamil Mroczkowski";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(256, 473);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(111, 20);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "kamil@kbejt.pl";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(391, 473);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(67, 20);
+            this.linkLabel2.TabIndex = 13;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Website";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 513);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btFolder);
+            this.Controls.Add(this.tbFolder);
+            this.Controls.Add(this.rbFolder);
+            this.Controls.Add(this.btFile);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.rbFile);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thunderbird Fix Mails Headers v1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbFile;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btFile;
+        private System.Windows.Forms.RadioButton rbFolder;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Button btFolder;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ ThunderbirdFixMailsHeaders/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThunderbirdFixMailsHeaders
+{
+    public partial class Form1 : Form
+    {
+        private DateTime dtstart;
+        private List<string> files = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbFile.Text = openFileDialog1.FileName;
+                rbFile.Checked = true;
+            }
+        }
+
+        private void btFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbFolder.Text = folderBrowserDialog1.SelectedPath;
+                rbFolder.Checked = true;
+            }
+        }
+
+        private void SL(string message, bool newline = false, bool datetime = false)
+        {
+            if (message == "")
+            {
+                return;
+            }
+            if (datetime)
+            {
+                tbStatus.Invoke((MethodInvoker)delegate
+                {
+                    tbStatus.AppendText(DateTime.Now.Hour.ToString().PadLeft(2, '0') + ":" +
+                        DateTime.Now.Minute.ToString().PadLeft(2, '0') + ":" +
+                        DateTime.Now.Second.ToString().PadLeft(2, '0') + " ");
+                });
+            }
+            tbStatus.Invoke((MethodInvoker)delegate
+            {
+                tbStatus.AppendText(message);
+            });
+            if (newline)
+            {
+                tbStatus.Invoke((MethodInvoker)delegate
+                {
+                    tbStatus.AppendText(Environment.NewLine);
+                });
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //fix
+            if (backgroundWorker1.CancellationPending == false)
+            {
+                SL("Start fix:", true, true);
+                backgroundWorker1.ReportProgress(0);
+                long count, size;
+                for (int i = 0; i < files.Count; i++)
+                {
+                    if (backgroundWorker1.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    else
+                    {
+                        groupBox1.Invoke((MethodInvoker)delegate
+                        {
+                            groupBox1.Text = "Status (file: " + (i + 1) + "/" + files.Count + "):";
+                        });
+                        SL("- file: " + files[i], true);
+                        if (File.Exists(files[i] + ".original"))
+                        {
+                            SL("Skip file: " + files[i] + " (.original exist!)", true);
+                            continue;
+                        }
+                        else
+                        {
+                            File.Move(files[i], files[i] + ".original");
+                        }
+                        using (StreamReader sr = new StreamReader(files[i] + ".original"))
+                        {
+                            count = 1;
+                            size = sr.BaseStream.Length;
+                            using (StreamWriter sw = new StreamWriter(files[i]))
+                            {
+                                while (!sr.EndOfStream)
+                                {
+                                    int c = sr.Read();
+                                    if (c == '\r' && sr.Peek() == '\r')
+                                    {
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        sw.Write((char)c);
+                                    }
+                                    count++;
+                                    if (count % 1000000 == 0)
+                                    {
+                                        backgroundWorker1.ReportProgress((int)(count * 100 / size));
+                                    }
+                                }
+                            }
+                        }
+                        if (File.Exists(files[i] + ".msf"))
+                        {
+                            File.Delete(files[i] + ".msf");
+                        }
+                        File.Delete(files[i] + ".original");
+                    }
+                }
+                SL("Time progress: " + TimeSpan.FromSeconds((DateTime.Now - this.dtstart).TotalSeconds).ToString(@"hh\:mm\:ss"), true, true);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btStart.Text = "Start";
+            groupBox1.Text = "Status:";
+            btStart.Enabled = true;
+            progressBar1.Value = 100;
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy == false)
+            {
+                if (MessageBox.Show("Before you start backup your files! Continue?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
+                {
+                    return;
+                }
+                this.files = new List<string>();
+                //file
+                if (rbFile.Checked)
+                {
+                    if (tbFile.Text == "")
+                    {
+                        MessageBox.Show("Please select file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        files.Add(tbFile.Text);
+                    }
+                }
+                //folder
+                if (rbFolder.Checked)
+                {
+                    if (tbFolder.Text == "")
+                    {
+                        MessageBox.Show("Please select folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        foreach (string file0 in Directory.GetFiles(tbFolder.Text).Select(Path.GetFileName))
+                        {
+                            if (file0.Contains(".") == false)
+                            {
+                                files.Add(tbFolder.Text + "\\" + file0);
+                            }
+                        }
+                    }
+                }
+                //program is running?
+                if (Process.GetProcessesByName("Thunderbird").Length > 0)
+                {
+                    MessageBox.Show("Please close Thunderbird.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                //list files
+                SL("Selected files:", true, true);
+                foreach (string file0 in files)
+                {
+                    SL("- " + file0, true);
+                }
+                dtstart = DateTime.Now;
+                progressBar1.Value = 0;
+                tbStatus.Text = string.Empty;
+                btStart.Text = "Cancel";
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                backgroundWorker1.CancelAsync();
+                btStart.Enabled = false;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("mailto:kamil@kbejt.pl?subject=Thunderbird%20Fix%20Mails%20Headers");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/kamilmroczkowski/ThunderbirdFixMailsHeaders");
+        }
+    }
+}
